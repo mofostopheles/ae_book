9e57772cc66a6458e65bacc618a6acca472f0bf9
Arlo Emerson <arloemerson@gmail.com>
2020-01-06 08:21:43 -0800

    Adding a common include

---
+++ dist/scripts/__common.jsx
@@ -0,0 +1,41 @@
+﻿// __common.jsx
+
+// Copyright © 2020, Arlo Emerson
+// arloemerson@gmail.com
+
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
+ * indexOf
+ */
+
+if(!Array.prototype.indexOf) 
+{
+    Array.prototype.indexOf = function(obj) 
+    {
+        for(var i=0; i<this.length; i++) 
+        {
+            if(obj===this[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    };
+}
+
+
+    
